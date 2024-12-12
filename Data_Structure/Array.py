e59195d201d04162a41370af1d799b119985e5f3
Chanchal Juyal(CJ) <51054068+juyal7@users.noreply.github.com>
2024-12-11 23:13:36 -0500

    it's amend

--- Data_Structure/Array.py
+++ Data_Structure/Array.py
@@ -33,13 +33,79 @@ class my_class:
         result=""
         for i in range(self.n):
             result+=str(self.A[i])+","
-        return "[+result[:-1]+]"
+        return "["+result[:-1]+"]"
 
     """Get item"""
+    def __getitem__(self, index):
+        if 0<=index<self.n:
+            return self.A[index]
+        else:
+            return "IndexError - Index out of range"
+
+    """Set item"""
+    def __setitem__(self, index, value):
+        if 0<=index<self.n:
+            self.A[index]=value
+        else:
+            return "IndexError - Index out of range"
+
+    """Delete item"""
+    def __delitem__(self, index):
+        if 0<=index<self.n:
+            for i in range(index, self.n-1):
+                self.A[i]=self.A[i+1]
+            self.n-=1
+        else:
+            return "IndexError - Index out of range"
+        if self.n==self.size//4:
+            self.__resize(self.size//2)
+            return "Resized"
+    """Clear"""
+    def clear(self):
+        self.n=0
+        self.size=1
+        return "Cleared"""
+    
+    """Find"""
+    def find(self, element):
+        for i in range(self.n):
+            if self.A[i]==element:
+                return i
+        return "Element not found"""
+
+    """Insert"""
+    def insert(self, index, element):
+        if 0<=index<self.n:
+            if self.n==self.size:
+                self.__resize(self.size*2)
+            for i in range(self.n, index, -1):
+                self.A[i]=self.A[i-1]
+            self.A[index]=element
+            self.n+=1
+        else:
+            return "IndexError - Index out of range"    
+    """Remove"""
+    def remove(self, element):
+        index=self.find(element)
+        if type(index)==int:
+            self.__delitem__(index)
+        else:
+            return index
+    """Pop"""
+    def pop(self, index=-1):
+        if 0<=index<self.n:
+            element=self.A[index]
+            self.__delitem__(index)
+            return element
 
 L=my_class()
 L.append(1)
 L.append(2)
 L.append(3)
 L.append(4)
-print(L)
+print
+
+
+#####
+########
+###
--- Sales_Analysis Project1/Sales_Analysis.py
+++ Sales_Analysis Project1/Sales_Analysis.py
@@ -5,3 +5,4 @@ import seaborn as sns
 data = pd.read_csv("C:/Users/Chanchal Juyal/Desktop/My Work/Learning Docs/AIML/Classes/Project2/AusApparalSales4thQrt2020.csv")
 print(data.head(10))
 print(data.tail(10))
+print(data.tail(15))
