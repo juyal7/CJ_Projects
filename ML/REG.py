a6f86a9db8521289f37ebfb7954a393b427948dd
Chanchal Juyal <juyalchanchal8@gmail.com>
2024-12-23 12:26:53 -0500

    Deleted unwanted files

---
+++ ML/REG.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data=pd.read_csv("C:/Users/Chanchal Juyal/Desktop/My Work/Learning Docs/AIML/Classes/ML/tvmarketing.csv")
+print(data.head(10))
